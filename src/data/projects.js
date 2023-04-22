b558c9895a4bf5d61f4a3caa45d4d09ad2bf582f
h4rasho <h4rasho@gmail.com>
2023-04-22 16:32:08 -0400

    add project component

---
+++ src/data/projects.js
@@ -0,0 +1,13 @@
+import monogatariApiProject from "../assets/monogatari-quotes.webp";
+
+export const PROJECTS = [
+  {
+    id: "monogatari_api",
+    title: "Monogatari Quotes API",
+    description:
+      "An API for the Monogatari Series, with search and filter options.",
+    image: monogatariApiProject,
+    repository: "https://github.com/H4rasho/monogatari-quotes",
+    web: "http://monogatari-quotes.vercel.app/",
+  },
+];
