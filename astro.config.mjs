498c79bf45a922ad9c5ce922dd7c73c81f6983c9
h4rasho <h4rasho@gmail.com>
2023-07-08 19:40:40 -0400

    remove preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import image from "@astrojs/image";
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image(), preact()]
+  integrations: [tailwind(), image()],
 });
