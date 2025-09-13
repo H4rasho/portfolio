11b5b8fa73eaf5746c3bb2b970648e8b26414b4a
h4rasho <h4rasho@gmail.com>
2025-09-13 19:13:05 -0300

    refactor code style for consistency and readability across various files, including ESLint and Prettier configurations, component imports, and project data

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
-import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+import image from '@astrojs/image'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), image()],
   vite: {
     ssr: {
-      noExternal: ["lucide-astro"],
+      noExternal: ['lucide-astro'],
     },
   },
-});
+})
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,31 +1,31 @@
-import monogatariApiProject from "../assets/monogatari-quotes.webp";
-import rickAndMorty from "../assets/rickandmorty.webp";
-import memoriceSpacial from "../assets/memorice_spacial.webp";
+import memoriceSpacial from '../assets/memorice_spacial.webp'
+import monogatariApiProject from '../assets/monogatari-quotes.webp'
+import rickAndMorty from '../assets/rickandmorty.webp'
 
 export const PROJECTS = [
   {
-    id: "monogatari_api",
-    title: "Monogatari Quotes API",
+    id: 'monogatari_api',
+    title: 'Monogatari Quotes API',
     description:
-      "An API for the Monogatari Series, with search and filter options.",
+      'An API for the Monogatari Series, with search and filter options.',
     image: monogatariApiProject,
-    repository: "https://github.com/H4rasho/monogatari-quotes",
-    web: "https://monogatari-quotes.vercel.app/",
+    repository: 'https://github.com/H4rasho/monogatari-quotes',
+    web: 'https://monogatari-quotes.vercel.app/',
   },
   {
-    id: "rick_and_morty",
-    title: "Rick and Morty Wiki",
+    id: 'rick_and_morty',
+    title: 'Rick and Morty Wiki',
     description:
-      "A project with SEO Optimization, using SSR and GraphQL query language. Search by Character and other options. Persistent data with LocalStorage",
+      'A project with SEO Optimization, using SSR and GraphQL query language. Search by Character and other options. Persistent data with LocalStorage',
     image: rickAndMorty,
-    repository: "https://github.com/H4rasho/Rick-and-Morty-App",
-    web: "https://rick-and-morty-app-1d6t.vercel.app/",
+    repository: 'https://github.com/H4rasho/Rick-and-Morty-App',
+    web: 'https://rick-and-morty-app-1d6t.vercel.app/',
   },
   {
-    id: "memorice_spacial",
-    title: "Memorice Spacial",
+    id: 'memorice_spacial',
+    title: 'Memorice Spacial',
     description:
-      "Memorice game with gobal state management. My first project with React",
+      'Memorice game with gobal state management. My first project with React',
     image: memoriceSpacial,
   },
-];
+]
